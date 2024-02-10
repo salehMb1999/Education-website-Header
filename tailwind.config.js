88d19e2fde1f63f9a1846d72e4bff3e309e51f44
salehmb1999 <salehmirbagheri78@gmail.com>
2024-02-10 17:03:52 +0330

    initializing the project and finished the nav

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "poppinsReg" : "poppinsReg",
+      "poppinsMed" : "poppinsMed",
+      "poppinsSemiBold" : "poppinsSemiBold",
+      "poppinsBold" : "poppinsBold"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
